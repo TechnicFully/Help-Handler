827cb723d69415518c82e27c68aeaf4875d873c2
Acinomatnas <keaton@keatonsquires.com>
2020-06-10 20:56:36 -0600

    Update help_handler_example.c

--- help_handler_example.c
+++ help_handler_example.c
@@ -9,7 +9,7 @@ int main(int argc, char** argv) {
     clock_t t;
     t = clock();
     
-	help_handler(argc, argv, "Usage: Test\n", NULL, HELP_MATCH_EXTRA_STRINGS);
+    help_handler(argc, argv, "Usage: Test\n", NULL, HELP_MATCH_EXTRA_STRINGS);
     
     t = clock() - t;
     double time_taken = ((double)t)/CLOCKS_PER_SEC; //in seconds
