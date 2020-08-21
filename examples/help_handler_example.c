6c123ed8c166c73a804c56cb32710f362aebda19
Acinomatnas <keaton@keatonsquires.com>
2020-08-21 14:06:27 -0600

    Small C version improvements

--- examples/help_handler_example.c
+++ examples/help_handler_example.c
@@ -9,11 +9,11 @@ int main(int argc, char** argv) {
     clock_t t;
     t = clock();
     
-    help_handler_config(1, 1, 1, "1.0.0");
-    help_handler_ver("2.0.0");
+    help_handler_config(1, 1, 1, "1.0.0\n");
+    help_handler_ver("2.0.0\n");
     help_handler(argc, argv, "Usage: Test\n", NULL);
     
     t = clock() - t;
-    double time_taken = ((double)t)/CLOCKS_PER_SEC; //in seconds
+    double time_taken = ((double)t)/CLOCKS_PER_SEC; //In seconds
     printf("help_handler() exited in %f seconds\n", time_taken);
 }
--- help_handler.h
+++ help_handler.h
@@ -87,19 +87,7 @@ int help_handler(int argc, char** argv, char* help, char* unknown_arg) {
         return EXIT_SUCCESS;
     }
     
-    char* help_lex[] = { "h", "-h", "--h",
-        "help", "-help","--help",
-        "hhelp", "heelp", "hellp", "helpp",
-        "-hhelp", "-heelp", "-hellp", "-helpp",
-        "--hhelp", "--heelp", "--hellp", "--helpp" };
-    char* ver_lex[] = { "v", "-v", "--v",
-        "version", "-version", "--version",
-        "vversion", "veersion", "verrsion", "verssion", "versiion", "versioon", "versionn",
-        "-vversion", "-veersion", "-verrsion", "-verssion", "-versiion", "-versioon", "-versionn",
-        "--vversion", "--veersion", "--verrsion", "--verssion", "--versiion", "--versioon", "--versionn" };
-    
-    
-    
+
     /****************/
     /* Error checks */
     /****************/
@@ -110,52 +98,67 @@ int help_handler(int argc, char** argv, char* help, char* unknown_arg) {
         return EXIT_FAILURE;
     } if (!help) {
         help = "No usage help is available\n"; }
-    if (!ver_glob) {
+    if (strlen(ver_glob) <= 0) {
         strcpy(ver_glob, "No version is available\n"); }
     
     
     if (argc > 128) {
-        if (argc > INT_MAX || argc < INT_MIN) {
+        if (argc > INT_MAX) {
             #ifndef HELP_IGNORE_WARN
-            fprintf(stderr, "ERROR:%s() argument count (argc) is outside the limit of int type\n", __func__);
+            fprintf(stderr, "ERROR:%s() argument count (argc) is larger than the limit of int type\n", __func__);
             fflush(stderr);
             #endif
             return EXIT_FAILURE; }
-    #ifndef HELP_IGNORE_WARN
-        else {
-            fprintf(stderr, "WARNING:%s() argument count (argc) is extremely large (128+)%s\n", __func__, warn); }
     } else if (argc < 1) {
-        fprintf(stderr, "WARNING:%s() argument count (argc) is 0 or less (should always be at least 1)%s\n", __func__, warn); }
-    if (CHAR_BIT != 8) {
-        fprintf(stderr, "Char type is not defined as 8 bits. This is untested and may result in issues and/or crashing\n%s\n", warn); }
-    fflush(stderr);
-    #else
+        if (argc < INT_MIN) {
+            #ifndef HELP_IGNORE_WARN
+            fprintf(stderr, "WARNING:%s() argument count (argc) is smaller than the limit of int type%s\n", __func__, warn);
+            #endif
+        } else {
+            #ifndef HELP_IGNORE_WARN
+            fprintf(stderr, "WARNING:%s() argument count (argc) is 0 or less (should always be at least 1)..%s\n", __func__, warn);
+            #endif
+            }
+        fflush(stderr);
+        return EXIT_FAILURE;
     }
-    #endif
+    if (CHAR_BIT != 8) {
+        fprintf(stderr, "Char type is not defined as 8 bits. This is untested and may result in issues and/or crashing%s\n", warn); }
+    fflush(stderr);
+
 
 
 
     /*******/
     /* Run */
     /*******/
+    char* help_lex[] = { "h", "-h", "--h",
+        "help", "-help","--help",
+        "hhelp", "heelp", "hellp", "helpp",
+        "-hhelp", "-heelp", "-hellp", "-helpp",
+        "--hhelp", "--heelp", "--hellp", "--helpp" };
+    char* ver_lex[] = { "v", "-v", "--v",
+        "version", "-version", "--version",
+        "vversion", "veersion", "verrsion", "verssion", "versiion", "versioon", "versionn",
+        "-vversion", "-veersion", "-verrsion", "-verssion", "-versiion", "-versioon", "-versionn",
+        "--vversion", "--veersion", "--verrsion", "--verssion", "--versiion", "--versioon", "--versionn" };
+    const int help_lex_element_count = sizeof(help_lex)/CHAR_BIT;
+    const int ver_lex_element_count = sizeof(ver_lex)/CHAR_BIT;
+
+
     if (extra_strings_glob != true) {
         ver_lex[0] = "";ver_lex[1] = "";ver_lex[2] = "";
         help_lex[0] = "";help_lex[1] = "";help_lex[2] = "";
     }
 
-
-    const int help_lex_element_count = sizeof(help_lex)/CHAR_BIT;
-    const int ver_lex_element_count = sizeof(ver_lex)/CHAR_BIT;
-
-
-    int i; for (i = 1; i < argc; i++) {
+    for (int i = 1; i < argc; i++) {
         if (argv[i] == NULL) {
-            fprintf(stderr, "ERROR:%s() argument count (argc) exceeds actual number of arguments (argv) or argument argv is NULL\n", __func__);
+            fprintf(stderr, "ERROR:%s() argument count (argc) exceeds actual number of arguments (argv) or argument argv[%d] is NULL\n", __func__, i);
             fflush(stderr);
             return EXIT_FAILURE;
         }
     
-        int j; for (j = 0; j < help_lex_element_count; j++) {
+        for (int j = 0; j < help_lex_element_count; j++) {
             #if defined _WIN32 || defined _WIN64
             int result = _stricmp(argv[i], help_lex[j]);
             #else
@@ -165,10 +168,10 @@ int help_handler(int argc, char** argv, char* help, char* unknown_arg) {
             if (result == 0) {
                 printf("%s", help);
                 fflush(stdout);
-                //printf("Matched %s with lex %s\n%s\n", argv[i], help_lex[j], help); //Debug
+                //printf("Matched %s with lex %s\n%s\n", argv[i], help_lex[j], help);  //Debug
                 return EXIT_SUCCESS; }
         
-        } int k; for (k = 0; k < ver_lex_element_count; k++) {
+        } for (int k = 0; k < ver_lex_element_count; k++) {
             #if defined _WIN32 || defined _WIN64
             int result = _stricmp(argv[i], ver_lex[k]);
             #else
@@ -178,7 +181,7 @@ int help_handler(int argc, char** argv, char* help, char* unknown_arg) {
             if (result == 0) {
                 printf("%s", ver_glob);
                 fflush(stdout);
-                //printf("Matched %s with lex %s\n%s\n", argv[i], ver_lex[k], help); //Debug
+                //printf("Matched %s with lex %s\n%s\n", argv[i], ver_lex[k], help);  //Debug
                 return EXIT_SUCCESS; }
         }
     }
