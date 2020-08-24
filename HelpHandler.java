d294e9a9709f60e4f5a2f46fccf1f3e3741dc3d6
Acinomatnas <keaton@keatonsquires.com>
2020-08-23 20:30:23 -0600

    Revert "Update README for Java version"
    This reverts commit 83395d50e10be6460afb370d8d6b44a5f0274f49.

--- HelpHandler.java
+++ HelpHandler.java
@@ -42,7 +42,7 @@ public class HelpHandler {
     }
 
 
-    public static void handle(String args[], String help, String unknownArgument) {
+    public static void handle(String args[], String help, String unknownArgument) throws Exception {
         if (help.length() <= 0) help = "No usage help is available";
         if (noArgHelpGlob == true && args.length <= 0) {
             System.out.println(help);
