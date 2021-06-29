a7dd98a8528e7df5fe483f888e946c89422ca82b
Inaff <58312640+Inaff@users.noreply.github.com>
2021-06-28 22:51:20 -0600

    Delete old files

--- examples/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-
-echo "Compiling C example"
-gcc main.c -Wall -Wextra -pedantic -std=c99 -o main_c
-echo "Compiling C++ example"
-g++ main.cpp -Wall -Wextra -pedantic -std=c++11 -o main_cpp
-echo "Compiling Java example"
-javac -cp "../" -d "./" main.java #--release 7
