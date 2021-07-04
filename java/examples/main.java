8afe949d50ee94ad45c003bf4550dd3d4d9f53a9
Inaff <58312640+Inaff@users.noreply.github.com>
2021-07-04 14:35:29 -0600

    Revert "Add Java version"
    This reverts commit 85a8fba4755aad2b17fc570ae854a11296cd33ed.

--- java/examples/main.java
+++
@@ -1,6 +0,0 @@
-public class main {
-    public static void main(final String args[]) throws Exception {
-        HelpHandler.version("1.0");
-        HelpHandler.handle(args, "Usage: Test"); //You can pass the version as the third argument here instead for convenience
-    }
-}
