6540ad9af5fce586a5b7a5de781d3015919ea8a2
Inaff <keaton@keatonsquires.com>
2021-11-09 16:10:45 -0700

    Update example1.py

--- python/examples/example1.py
+++ python/examples/example1.py
@@ -1,5 +1,5 @@
 import os, sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) #Needed to find the help_handler file in the parent directory
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) #Needed to find help_handler.py file in the parent directory
 from help_handler import *
 
 
