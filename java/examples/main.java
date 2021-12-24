a55d01bfb6600a4cc23c0ce42ccca96243c925de
Inaff <keaton@keatonsquires.com>
2021-12-24 13:32:53 -0700

    Update main.java

--- java/examples/main.java
+++ java/examples/main.java
@@ -1,6 +1,10 @@
 public class main {
-    public static void main(final String args[]) throws Exception {
-        HelpHandler.version("1.0");
-        HelpHandler.handle(args, "Usage: Test"); //You can pass the version as the third argument here instead for convenience
+    public static void main(final String args[]) {
+        try {
+            HelpHandler.version("1.0");
+            HelpHandler.handle(args, "Usage: Example help");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
