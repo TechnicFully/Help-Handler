0c0fef8cbe968ff228526e11851c557d1fde8eb5
TechnicFully <58312640+TechnicFully@users.noreply.github.com>
2022-01-29 22:10:29 -0700

    Delete Compile.bat

--- c/Compile.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-ECHO Compiling C example...
-gcc examples/example1.c -Wall -Wextra -pedantic -Wconversion -fstack-protector-all -fstack-protector-strong -D_FORTIFY_SOURCE=2 -Wformat=2 -Wshadow -fwrapv -Werror=implicit-function-declaration -Wunreachable-code -g3 -std=c11 -o example1
-PAUSE
