c4610ef8a480e3acf6ae146a4c153ae23af40a91
TechnicFully <58312640+TechnicFully@users.noreply.github.com>
2022-05-25 15:38:26 -0600

    Update help_handler.h

--- c/help_handler.h
+++ c/help_handler.h
@@ -32,8 +32,9 @@
  * These should be the only relevant variables to any library user not trying to change functionality
  */
 
-#define HELP_HANDLER_VERSION_MAJOR 0.1
-#define HELP_HANDLER_VERSION_MINOR 0
+#define HELP_HANDLER_VERSION_MAJOR 0
+#define HELP_HANDLER_VERSION_MINOR 1
+#define HELP_HANDLER_VERSION_BUGFIX 0
 #define HELP_HANDLER_VERSION_STR "0.1.0"
 
 //Parameters for help_handler_config()
