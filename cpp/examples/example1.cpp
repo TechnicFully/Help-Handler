1764491c0f01dc8900c84fcb9de0042bc8ee55a8
TechnicFully <58312640+TechnicFully@users.noreply.github.com>
2022-06-20 21:22:56 -0600

    Update example1.cpp

--- cpp/examples/example1.cpp
+++ cpp/examples/example1.cpp
@@ -8,7 +8,6 @@
 
 int main(int argc, char** argv) {
     try {
-        helpHandler::config(ENABLE_HYPHENS_ONLY|DISABLE_MATCH_HYPHENS);
         helpHandler::handle(argc, argv, "usage: help dialogue example");
     } catch(const std::exception& e) { std::cout << e.what() << std::endl; }
     
