6bee32d398fb10f825cbb0978708204b88485d2c
TechnicFully <58312640+TechnicFully@users.noreply.github.com>
2023-11-18 17:18:56 -0700

    Update comment

--- cpp/examples/example3.cpp
+++ cpp/examples/example3.cpp
@@ -8,7 +8,7 @@
 
 int main(int argc, char** argv) {
     try {
-        helpHandler::name("TestApp"); //This must also come before helpHandler::handle, however it doesn't matter if it's called/set before or after helpHandler::version()
+        helpHandler::name("TestApp"); //This sets the application name and must also come before helpHandler::handle(), however it doesn't matter if it's called before or after helpHandler::version()
         helpHandler::version("1.0");
         helpHandler::handle(argc, argv, "usage: help dialogue example");
     } catch(const std::exception& e) { std::cout << e.what(); }
--- cpp/examples/example5.cpp
+++ cpp/examples/example5.cpp
@@ -9,7 +9,7 @@
 int main(int argc, char** argv) {
     try {
         helpHandler::info("TestApp", "1.0"); //Also note that this is optional
-        helpHandler::handleFile(argc, argv, "example_dialoge_file.txt"); //Similar to helpHandler::handle(), this will process arguments once called as well, but gets the help dialogue from a text file
+        helpHandler::handleFile(argc, argv, "example_dialoge_file.txt"); //Like helpHandler::handle(), this will process arguments once called as well, but gets the help dialogue from a text file
     } catch(const std::exception& e) { std::cout << e.what(); }
 
     return EXIT_SUCCESS;
