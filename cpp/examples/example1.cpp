94007676e411b6bb3de24865faf4d7ad6ca07e89
TechnicFully <58312640+TechnicFully@users.noreply.github.com>
2023-12-18 19:53:43 -0700

    Update example1.cpp

--- cpp/examples/example1.cpp
+++ cpp/examples/example1.cpp
@@ -9,7 +9,7 @@
 
 int main(int argc, char** argv) {
     try {
-        int rval = helpHandler::handle(argc, argv, "usage: help dialogue example"); //Set help dialogue and process arguments
+        helpHandler::handle(argc, argv, "usage: help dialogue example"); //Set help dialogue and process arguments
     } catch(const std::exception& e) { std::cout << e.what(); }
     
     return EXIT_SUCCESS;
