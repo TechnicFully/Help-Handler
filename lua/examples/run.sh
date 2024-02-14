bbbe6874f34692c4e6d3460deae5ec6b20315f5d
TechnicFully <58312640+TechnicFully@users.noreply.github.com>
2024-02-13 22:50:17 -0700

    Create run.sh

---
+++ lua/examples/run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+cd "$(dirname "$0")"
+
+lua example1.lua
+lua example2.lua
+lua example3.lua
+lua example4.lua
+lua example5.lua
+lua example6.lua
+lua example7.lua
+lua example8.lua
+lua example9.lua
