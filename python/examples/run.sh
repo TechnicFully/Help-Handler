9c5adcdb05449cdb607410a1e901a2715a092ce1
TechnicFully <58312640+TechnicFully@users.noreply.github.com>
2024-02-13 19:11:29 -0700

    Create run.sh

---
+++ python/examples/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+
+cd "$(dirname "$0")"
+
+python3 example1.py
+python3 example2.py
+python3 example3.py
+python3 example4.py
+python3 example5.py
+python3 example6.py
+python3 example7.py
+python3 example8.py
